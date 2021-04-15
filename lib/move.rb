75ae2e3c73066108297653abb8815c6a49006e47
colorful-state-34cc <seth623@gmail.com>
2021-04-15 18:02:09 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,5 +1,8 @@
 board = [" "," "," "," "," "," "," "," "," "]
+<<<<<<< HEAD
 
+=======
+>>>>>>> b12d76f0b06c5ebc9aaa0fd70df7f63dad18a4a5
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -17,5 +20,11 @@ end
 
 #move
 def move(array, index, value = 'X')
+<<<<<<< HEAD
     array[index] = value
+=======
+    input_to_index(index)
+    array[index] = value
+    display_board(array)
+>>>>>>> b12d76f0b06c5ebc9aaa0fd70df7f63dad18a4a5
 end
